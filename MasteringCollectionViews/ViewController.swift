77b864fa84c82be45cdb652c6e7025599379a14f
Simran Preet Singh <simran5590@gmail.com>
2022-06-21 06:58:32 -0700

    A basic collectio view

--- MasteringCollectionViews/ViewController.swift
+++ MasteringCollectionViews/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  var data = ["🍔", "🌭", "🌮", "🌯", "🥙", "🥗", "🥪", "🍕", "🍟", "🍱", "🧆", "🍲", "🍛", "🍜", "🍣", "🍿", "🥟",
+              "🍳", "🥞", "🧇", "🍪", "🍩", "🧁", "🍦", "🍮", "🥧", "🍰", "🥡"].shuffled()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    collectionView.delegate = self
+    collectionView.dataSource = self
   }
+}
 
-
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    data.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    if let label = cell.viewWithTag(100) as? UILabel {
+      label.text = data[indexPath.item]
+    }
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    CGSize(width: 180, height: 180)
+  }
 }
 
